4ca6c331a1821620a18d05ce4c6a1c4a336325d8
Yuseunggeun <yuu3730@naver.com>
2023-11-07 22:44:56 +0900

    Initial spring-boot project

---
+++ src/main/java/com/example/baekjoon_recommendation_server/BaekjoonRecommendationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.baekjoon_recommendation_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaekjoonRecommendationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaekjoonRecommendationServerApplication.class, args);
+	}
+
+}
