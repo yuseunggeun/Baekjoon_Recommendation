b5a4dc1e4fc9bc8793403196bcc9acfdb2a91928
Yuseunggeun <yuu3730@naver.com>
2023-11-08 23:36:12 +0900

    Feat: JSON 형식으로 검색결과 전달

--- src/main/java/com/example/baekjoon_recommendation_server/web/controller/ProblemController.java
+++ src/main/java/com/example/baekjoon_recommendation_server/web/controller/ProblemController.java
@@ -3,7 +3,10 @@ package com.example.baekjoon_recommendation_server.web.controller;
 
 import java.net.URI;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,10 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.example.baekjoon_recommendation_server.converter.SearchConverter;
 import com.example.baekjoon_recommendation_server.repository.ProblemRepository;
 import com.example.baekjoon_recommendation_server.service.ProblemService;
 import com.example.baekjoon_recommendation_server.web.dto.SearchDto;
+import com.example.baekjoon_recommendation_server.web.dto.base.DefaultRes;
 
 import lombok.RequiredArgsConstructor;
 
@@ -35,22 +38,24 @@ public class ProblemController {
 
 	@GetMapping("/search")
 	public ResponseEntity search(@RequestBody SearchDto request){
-		// try{
-			//User user = ```
+		try{
 			String url = problemService.getSearchQuery(request);
 
-			System.out.println(url);
+			//System.out.println(url);
 
 			URI uri = UriComponentsBuilder
 				.fromUriString("https://solved.ac/api/v3/search/problem")
 				.queryParam("query", url)
 				.encode().build().toUri();
-			System.out.println(uri);
+			//System.out.println(uri);
 
 			ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
-			return response;
-		// } catch (Exception e) {
-		//
-		// }
+			JSONParser parser = new JSONParser();
+			JSONObject jsonObject = (JSONObject) parser.parse(response.getBody());
+			return new ResponseEntity(DefaultRes.res(200, "search_success", jsonObject), HttpStatus.OK);
+
+		} catch (Exception e) {
+			return new ResponseEntity(e, HttpStatus.BAD_REQUEST);
+		}
 	}
 }
