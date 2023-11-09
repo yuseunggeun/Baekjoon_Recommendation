78d64b7ffa4ec70dc1e2b527c4b924f04240968e
Yuseunggeun <yuu3730@naver.com>
2023-11-09 13:24:26 +0900

    Update: /problem/search GET -> POST

--- src/main/java/com/example/baekjoon_recommendation_server/web/controller/ProblemController.java
+++ src/main/java/com/example/baekjoon_recommendation_server/web/controller/ProblemController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,9 +39,10 @@ public class ProblemController {
 		return "test";
 	}
 
-	@GetMapping("/search")
+	@PostMapping("/search")
 	public ResponseEntity search(@RequestBody SearchDto request){
 		try{
+			System.out.println(request.getMinDifficulty());
 			String url = problemService.getSearchQuery(request);
 
 			//System.out.println(url);
