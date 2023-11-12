0cdc2e68dc2456b13474a1ac138b1702da09bd13
Yuseunggeun <yuu3730@naver.com>
2023-11-12 23:01:15 +0900

    Feat: response message, status code 추가

---
+++ src/main/java/com/example/baekjoon_recommendation_server/exception/ResponseMessage.java
@@ -0,0 +1,8 @@
+package com.example.baekjoon_recommendation_server.exception;
+
+public class ResponseMessage {
+
+	public static final String PROBLEM_SEARCH_SUCCESS = "문제 검색 성공";
+	public static final String PROBLEM_DETAIL_SUCCESS = "문제 세부정보 불러오기 성공";
+
+}
